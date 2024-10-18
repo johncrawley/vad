8d9e715f8c6ff44f57cbeb93643f5a468ff109a4
john <johncrawley1@gmail.com>
2024-10-18 19:28:44 +0100

    Adding back the configuration to generate minified worklet bundles.

--- packages/web/webpack.config.worklet.js
+++ packages/web/webpack.config.worklet.js
@@ -1,3 +1,16 @@
+const prod = { mode: "production", suffix: "min" }
+const dev = { mode: "development", suffix: "dev" }
+
+const workletConfig = ({ mode, suffix }) => {
+  return {
+    mode,
+    entry: { worklet: "./dist/worklet.js" },
+    output: {
+      filename: `vad.worklet.bundle.${suffix}.js`,
+    },
+  }
+}
+
 const nonMinified = () => {
   return {
     mode: "none",
@@ -8,4 +21,4 @@ const nonMinified = () => {
   }
 }
 
-module.exports = nonMinified
+module.exports = [workletConfig(dev), workletConfig(prod), nonMinified]
